9ffa25f746970e267c4ef9489c15c3f732c56f4f
tripp <tripp.bridges@yahoo.com>
2013-12-11 16:36:44 -0800

    Initial files for stockindicators.  	new file:   src/gallery-charts-stockindicators/build.json  	new file:   src/gallery-charts-stockindicators/js/Crosshair.js  	new file:   src/gallery-charts-stockindicators/js/Gridlines.js  	new file:   src/gallery-charts-stockindicators/js/StockIndicatorsChart.js  	new file:   src/gallery-charts-stockindicators/js/StockIndicatorsLegend.js  	new file:   src/gallery-charts-stockindicators/meta/gallery-charts-stockindicators.json  	new file:   src/gallery-charts-stockindicators/tests/unit/assets/charts-stockindicators-tests.js  	new file:   src/gallery-charts-stockindicators/tests/unit/charts-stockindicators.html

---
+++ src/gallery-charts-stockindicators/js/Crosshair.js
@@ -0,0 +1,125 @@
+/**
+ * Allows for the creation of a visualization based on financial
+ * indicators..
+ *
+ * @module charts-stockindicators
+ */
+Y.Crosshair = function() {
+    this.initializer.apply(this, arguments);
+};
+Y.Crosshair.prototype = {
+    /**
+     * Builds the crosshair.
+     *
+     * @method initializer
+     * @protected
+     */
+    initializer: function(cfg) {
+        var graphic = new Y.Graphic({
+                render: cfg.render,
+                autoDraw: false,
+                width: cfg.width,
+                height: cfg.height,
+                x: cfg.x,
+                y: cfg.y
+            }),
+            width = cfg.width,
+            height = cfg.height,
+            series = cfg.series,
+            graph,
+            category = cfg.category,
+            yline,
+            i,
+            len = series.length;
+        yline = graphic.addShape({
+            shapeRendering: "crispEdges",
+            type: "path",
+            stroke: category.stroke
+        }).moveTo(0, 0).lineTo(0, height).end();
+        this._xcoords = category.coords;
+        this._yline = yline;
+        this.width = cfg.width;
+        this.height = cfg.height;
+        if(series) {
+            for(i = 0; i < len; i = i + 1) {
+                graph = series[i];
+                if(graph.line) {
+                    graph.xLine = graphic.addShape({
+                        shapeRendering: "crispEdges",
+                        type: "path",
+                        stroke: graph.stroke,
+                        fill: graph.fill
+                    }).moveTo(0, 0).lineTo(width, 0).end();
+                }
+                if(graph.marker) {
+                    graph.marker.y = graph.marker.height/-2;
+                    graph.marker.x = graph.marker.width/-2;
+                    graph.marker.type = graph.marker.type || graph.marker.shape;
+                    graph.marker = graphic.addShape(graph.marker);
+                }
+            }
+            this._series = series;
+        }
+        this._xy = graphic.getXY();
+        this.graphic = graphic;
+    },
+
+    /**
+     * Updates the position of the crosshair.
+     *
+     * @method setTarget
+     * @param {Number} pageX The x-coordinate to map in which to map the crosshair.
+     */
+    setTarget: function(pageX) {
+        var xy = this._xy,
+            x = pageX - xy[0],
+            y,
+            series = this._series,
+            graph,
+            i,
+            index = Math.floor((x / this.width) * this._xcoords.length),
+            len = series.length;
+        this._yline.set("transform", "translate(" + x + ")");
+        if(series) {
+            for(i = 0; i < len; i = i + 1) {
+                graph = series[i];
+                y = graph.coords[index];
+                if(graph.marker) {
+                    graph.marker.set("transform", "translate(" + x + ", " + y + ")");
+                }
+                if(graph.line) {
+                    graph.line.set("transform", "translate(" + x + ", " + y + ")");
+                }
+            }
+        }
+        this.graphic._redraw();
+    },
+
+    /**
+     * Removes all elements of the crosshair.
+     *
+     * @method destroy
+     */
+    destroy: function() {
+        var series = this._series,
+            yline = this._yline,
+            graph,
+            i,
+            len;
+        if(series) {
+            len = series.length;
+            for(i = 0; i < len; i = i + 1) {
+                graph = series[i];
+                if(graph.marker) {
+                    graph.marker.get("graphic").destroy();
+                }
+                if(graph.line) {
+                    graph.line.get("graphic").destroy();
+                }
+                if(yline) {
+                    yline.get("graphic").destroy();
+                }
+            }
+        }
+    }
+};
