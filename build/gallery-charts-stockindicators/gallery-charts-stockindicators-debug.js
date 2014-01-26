007e2dd6158674dd1bed9c2da3161ee2e7af8597
tripp <tripp.bridges@yahoo.com>
2014-01-25 18:07:07 -0800

    Build files.

--- build/gallery-charts-stockindicators/gallery-charts-stockindicators-debug.js
+++ build/gallery-charts-stockindicators/gallery-charts-stockindicators-debug.js
@@ -6,117 +6,6 @@ YUI.add('gallery-charts-stockindicators', function (Y, NAME) {
  *
  * @module gallery-charts-stockindicators
  */
-var defaultAxisLabelFormat = {
-    value: null
-};
-
-Y.CategoryAxisBase.ATTRS.labelFormat = defaultAxisLabelFormat;
-Y.CategoryAxis.ATTRS.labelFormat = defaultAxisLabelFormat;
-//patch CartesianSeries destructor bug
-Y.CartesianSeries.prototype.destructor = function() {
-    if(this.get("rendered"))
-    {
-        if(this._xDataReadyHandle)
-        {
-            this._xDataReadyHandle.detach();
-        }
-        if(this._xDataUpdateHandle)
-        {
-            this._xDataUpdateHandle.detach();
-        }
-        if(this._yDataReadyHandle)
-        {
-            this._yDataReadyHandle.detach();
-        }
-        if(this._yDataUpdateHandle)
-        {
-            this._yDataUpdateHandle.detach();
-        }
-        if(this._xAxisChangeHandle)
-        {
-            this._xAxisChangeHandle.detach();
-        }
-        if(this._yAxisChangeHandle)
-        {
-            this._yAxisChangeHandle.detach();
-        }
-    }
-};
-
-if(Y.VMLShape) {
-    Y.VMLShape.ATTRS.stroke.setter = function(val) {
-        var i,
-            stroke,
-            wt,
-            tmpl = this.get("stroke") || this._getDefaultStroke();
-        if(val)
-        {
-            if(val.hasOwnProperty("weight"))
-            {
-                wt = parseInt(val.weight, 10);
-                if(!isNaN(wt))
-                {
-                    val.weight = wt;
-                }
-            }
-            for(i in val)
-            {
-                if(val.hasOwnProperty(i))
-                {
-                    tmpl[i] = val[i];
-                }
-            }
-        }
-        if(tmpl.color && tmpl.color.toLowerCase().indexOf("rgba") > -1)
-        {
-           tmpl.opacity = Y.Color._getAlpha(tmpl.color);
-           tmpl.color =  Y.Color.toHex(tmpl.color);
-        }
-        stroke = tmpl;
-        this._strokeFlag = true;
-        return stroke;
-    };
-    Y.VMLShape.ATTRS.fill.setter = function(val) {
-        var i,
-            fill,
-            tmpl = this.get("fill") || this._getDefaultFill();
-
-        if(val)
-        {
-            //ensure, fill type is solid if color is explicitly passed.
-            if(val.hasOwnProperty("color"))
-            {
-                val.type = "solid";
-            }
-            for(i in val)
-            {
-                if(val.hasOwnProperty(i))
-                {
-                    tmpl[i] = val[i];
-                }
-            }
-        }
-        fill = tmpl;
-        if(fill && fill.color)
-        {
-            if(fill.color === undefined || fill.color === "none")
-            {
-                fill.color = null;
-            }
-            else
-            {
-                if(fill.color.toLowerCase().indexOf("rgba") > -1)
-                {
-                    fill.opacity = Y.Color._getAlpha(fill.color);
-                    fill.color =  Y.Color.toHex(fill.color);
-                }
-            }
-        }
-        this._fillFlag = true;
-        return fill;
-    };
-}
-
 /**
  * Provides functionality for a crosshair.
  *
@@ -1712,7 +1601,7 @@ Y.StockIndicatorsSpark.prototype = {
             alwaysShowZero: false
         });
         bb.style.position = "absolute";
-        bb.style.display = "inlineBlock";
+        Y.DOM.setStyle(bb, "inlineBlock");
         cb.style.position = "relative";
         render = document.getElementById(render);
         render.appendChild(bb);
@@ -1776,6 +1665,7 @@ Y.StockIndicatorsSpark.prototype = {
 
 }, '@VERSION@', {
     "requires": [
+        "escape",
         "graphics-group",
         "axis-numeric",
         "axis-category",
