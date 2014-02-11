eb9f69a8f3e19da9e95d1f1bb771d943022a7ebc
tripp <tripp.bridges@yahoo.com>
2014-02-11 15:06:45 -0800

    Build files.

--- build/gallery-charts-stockindicators/gallery-charts-stockindicators-debug.js
+++ build/gallery-charts-stockindicators/gallery-charts-stockindicators-debug.js
@@ -1,5 +1,52 @@
 YUI.add('gallery-charts-stockindicators', function (Y, NAME) {
 
+/**
+ * Contains logic for rendering an intraday axis.
+ */
+Y.IntradayAxis = function() {
+    Y.IntradayAxis.superclass.constructor.apply(this, arguments);
+};
+Y.IntradayAxis.ATTRS = {
+    /**
+     * The granularity of the data in the axis.
+     *
+     * @attribute dataGranularity
+     * @type String
+     */
+    dataGranularity: {
+        lazyAdd: false
+    }
+};
+Y.IntradayAxis.NAME = "intradayAxis";
+Y.extend(Y.IntradayAxis, Y.CategoryAxis, {
+    /**
+     * Getter method for maximum attribute.
+     *
+     * @method _maximumGetter
+     * @return Number
+     * @private
+     */
+    _maximumSetter: function(val) {
+        var max = val,
+            dataGranularity = this.get("dataGranularity"),
+            interval,
+            allData = this.get("dataProvider"),
+            last = allData[allData.length - 1].Timestamp;
+        if(dataGranularity) {
+            interval = parseFloat(dataGranularity) * 60000;
+            max = new Date(max).valueOf();
+            last = new Date(last).valueOf();
+
+            if(max > last) {
+                while(max > last) {
+                    last = last + interval;
+                    allData.push({Timestamp: last});
+                }
+                this.set("dataProvider", allData);
+            }
+        }
+    }
+});
 Y.VolumeColumn = function() {
     Y.VolumeColumn.superclass.constructor.apply(this, arguments);
 };
@@ -1540,7 +1587,8 @@ Y.StockIndicatorsChart = Y.Base.create("stockIndicatorsChart",  Y.Widget, [Y.Ren
         numeric: Y.NumericAxis,
         numericbase: Y.NumericAxisBase,
         category: Y.CategoryAxis,
-        categorybase: Y.CategoryAxisBase
+        categorybase: Y.CategoryAxisBase,
+        intraday: Y.IntradayAxis
     },
 
     _adjustForInnerLabels: function(config) {
