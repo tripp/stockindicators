156c3d0b936aa5206f27fb1273e4990a84185a4d
tripp <tripp.bridges@yahoo.com>
2014-02-12 22:28:45 -0800

    Build files.

--- build/gallery-charts-stockindicators/gallery-charts-stockindicators.js
+++ build/gallery-charts-stockindicators/gallery-charts-stockindicators.js
@@ -58,8 +58,8 @@ Y.extend(Y.VolumeColumn, Y.RangeSeries, {
         var valueData = this.get("yAxis").get("dataProvider"),
             xcoords = this.get("xcoords"),
             volumeCoords = this.get("ycoords"),
-            positivePath = this.get("positivePath"),
-            negativePath = this.get("negativePath"),
+            upPath = this.get("upPath"),
+            downPath = this.get("downPath"),
             len = xcoords.length,
             i,
             styles = this.get("styles"),
@@ -72,18 +72,27 @@ Y.extend(Y.VolumeColumn, Y.RangeSeries, {
             left,
             height,
             selectedPath,
-            threshold = this.get("threshold");
-        styles.positive.fill.opacity = styles.positive.fill.alpha;
-        styles.negative.fill.opacity = styles.negative.fill.alpha;
-        positivePath.set(styles.positive);
-        negativePath.set(styles.negative);
-        positivePath.clear();
-        negativePath.clear();
+            previousClose = this.get("previousClose"),
+            hasUpPath = false,
+            hasDownPath = false,
+            drawInBackground = styles.drawInBackground;
+        styles.upPath.fill.opacity = styles.upPath.fill.alpha;
+        styles.downPath.fill.opacity = styles.downPath.fill.alpha;
+        upPath.set(styles.upPath);
+        downPath.set(styles.downPath);
+        if(drawInBackground) {
+            upPath.toBack();
+            downPath.toBack();
+        }
+        upPath.clear();
+        downPath.clear();
         for(i = 0; i < len; i = i + 1) {
-            if(threshold) {
-                selectedPath = valueData[i].close < threshold ? negativePath : positivePath;
+            if(previousClose && valueData[i].close < previousClose) {
+                selectedPath = downPath;
+                hasDownPath = true;
             } else {
-                selectedPath = positivePath;
+                selectedPath = upPath;
+                hasUpPath = true;
             }
             left = xcoords[i] - halfwidth;
             top = volumeCoords[i];
@@ -91,9 +100,14 @@ Y.extend(Y.VolumeColumn, Y.RangeSeries, {
             if(height > 0 && !isNaN(left) && !isNaN(top)) {
                 selectedPath.drawRect(left, top, width, height);
             }
+            previousClose = valueData[i].close;
         }
-        positivePath.end();
-        negativePath.end();
+        if(hasUpPath) {
+            upPath.end();
+        }
+        if(hasDownPath) {
+            downPath.end();
+        }
     },
 
     /**
@@ -105,8 +119,8 @@ Y.extend(Y.VolumeColumn, Y.RangeSeries, {
      */
     _toggleVisible: function(visible)
     {
-        this.get("positivePath").set("visible", visible);
-        this.get("negativePath").set("visible", visible);
+        this.get("upPath").set("visible", visible);
+        this.get("downPath").set("visible", visible);
     },
 
 
@@ -121,7 +135,7 @@ Y.extend(Y.VolumeColumn, Y.RangeSeries, {
     _getDefaultStyles: function()
     {
         var styles = {
-            positive: {
+            upPath: {
                 shapeRendering: "crispEdges",
                 fill: {
                     color: "#00aa00",
@@ -133,7 +147,7 @@ Y.extend(Y.VolumeColumn, Y.RangeSeries, {
                     weight: 0
                 }
             },
-            negative: {
+            downPath: {
                 shapeRendering: "crispEdges",
                 fill: {
                     color: "#aa0000",
@@ -144,7 +158,8 @@ Y.extend(Y.VolumeColumn, Y.RangeSeries, {
                     alpha: 1,
                     weight: 0
                 }
-            }
+            },
+            drawInBackground: true
         };
         return this._mergeStyles(styles, Y.VolumeColumn.superclass._getDefaultStyles());
     }
@@ -180,21 +195,42 @@ Y.extend(Y.VolumeColumn, Y.RangeSeries, {
                 if(!this.get("rendered")) {
                     this.set("rendered", true);
                 }
-                this.set("positivePath", val.addShape({
+                this.set("upPath", val.addShape({
                    type: "path"
                 }));
-                this.set("negativePath", val.addShape({
+                this.set("downPath", val.addShape({
                    type: "path"
                 }));
                 return val;
             }
         },
 
-        positivePath: {},
+        /**
+         * The path element in which columns higher that the previous are drawn.
+         * This attribute is created by the instance.
+         *
+         * @attribute upPath
+         * @type Path
+         */
+        upPath: {},
 
-        negativePath: {},
+        /**
+         * The path element in which columns lower that the previous are drawn.
+         * This attribute is created by the instance.
+         *
+         * @attribute downPath
+         * @type Path
+         */
+        downPath: {},
 
-        threshold: {
+        /**
+         * Attribute for the previous close value. This value represents the last
+         * close value before the start of the rendered data set.
+         *
+         * @attribute previousClose
+         * @type Number
+         */
+        previousClose: {
             lazyAdd: false
         }
     }
@@ -1370,9 +1406,7 @@ Y.StockIndicatorsChart = Y.Base.create("stockIndicatorsChart",  Y.Widget, [Y.Ren
                         if(indicatorType[valueIter] === "multipleline" && config.showThreshold) {
                             seriesConfig.thresholds = [parseFloat(indicator.previousClose)];
                         } else if(indicatorType[valueIter] === "volumecolumn") {
-                            if(config.showThreshold) {
-                                seriesConfig.threshold = parseFloat(indicator.previousClose);
-                            }
+                            seriesConfig.previousClose = parseFloat(indicator.previousClose);
                             seriesConfig.yAxis = new Y.NumericAxisBase(indicator.yAxis);
                         }
                     }
@@ -1407,16 +1441,14 @@ Y.StockIndicatorsChart = Y.Base.create("stockIndicatorsChart",  Y.Widget, [Y.Ren
             switch(series.type) {
                 case "volumecolumn" :
                     series.styles = {
-                        positive: {
+                        upPath: {
                             fill: {
-                                color: colors.priceUp,
-                                alpha: 0.5
+                                color: colors.volumeColumnUp
                             }
                         },
-                        negative: {
+                        downPath: {
                             fill: {
-                                color: colors.priceDown,
-                                alpha: 0.5
+                                color: colors.volumeColumnDown
                             }
                         },
                         padding: {
